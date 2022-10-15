455c8b2421594703bdc47089e1bda6f76e1ee76f
Jamelame45 <114234222+Jamelame45@users.noreply.github.com>
2022-10-15 13:37:21 +0700

    Add files via upload

---
+++ src/pages/About.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+        <Navbar />
+
+        <section class="about">
+
+            <h1 class="heading"> about <span>me</span> </h1>
+
+            <div class="row">
+
+                <div class="info-container">
+
+                    <h1>Manga Go</h1>
+
+                    <div class="box-container">
+
+                        <div class="box">
+                            <h3> <span>name : </span>{{name}}</h3>
+                            <h3> <span>founded : </span>{{age}}</h3>
+                            <h3> <span>email : </span>{{email}}</h3>
+                        </div>
+
+                   
+
+                    </div>
+
+                    <a href="/profilewithcss/files/resume.pdf" class="btn"> download CV <i class="fas fa-download"></i>
+                    </a>
+
+                </div>
+
+                <div class="count-container">
+
+                   
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <!-- about section ends -->
+
+        <!-- skills section starts  -->
+
+        <section class="skills">
+
+            <h1 class="heading"> <span>my</span> Manga and Anime </h1>
+
+            <div class="box-container">
+                <div class="box">
+                    <img src="images/action.jpg">
+                    <h3>Action</h3>
+                </div>
+                <div class="box">
+                    <img src="images/adventure.jpg">
+                    <h3>adventure</h3>
+                </div>
+                <div class="box">
+                    <img src="images/drama.webp">
+                    <h3>drama</h3>
+                </div>
+                <div class="box">
+                    <img src="images/romance.webp">
+                    <h3>romance</h3>
+                </div>
+                <div class="box">
+                    <img src="images/horror.jpg">
+                    <h3>horror</h3>
+                </div>
+                <div class="box">
+                    <img src="images/other.jpg">
+                    <h3>other</h3>
+                </div>
+            </div>
+
+        </section>
+
+        <!-- skills section ends -->
+
+        <!-- education section starts  -->
+
+        <section class="education">
+
+
+            <div class="box-container">
+
+                <div class="box" v-for="e in study" :key="e.years">
+                    <i class="fas fa-briefcase"></i>
+                    <span>{{e.years}}</span>
+                    <h3>{{e.school}}</h3>
+                    <p>{{e.jobs}}</p>
+                </div>
+
+            </div>
+
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+    components: { Navbar },
+    data() {
+        return {
+            name: 'Manga Go',
+            age: '15/10/2023',
+            email: 'Natthaphol.lapc@bu.ac.th',
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Navbar />
+        <section class="home">
+            <div class="image">
+                <img src="images/profile.webp" alt="">
+            </div>
+            <div class="content">
+                <h3>Hi, Wellcome to {{name}}</h3>
+                <span>{{motto}}</span>
+                <p>{{content}}</p>
+                <router-link to="/about" class="btn"> about me <i class="fas fa-user"></i> </router-link>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar },
+    data() {
+        return {
+            name: 'Manga Go',
+            motto: 'Manga is LIFE',
+            content: ' We have many manga to choose from.',
+        }
+}
+}
+</script>
+
+<style>
+
+</style>
